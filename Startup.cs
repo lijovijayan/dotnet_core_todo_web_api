0d80c83f693024b44ae3646bdff65d18c7330f73
lijovijayan <lijovijayan00@gmail.com>
2020-10-20 21:58:29 +0530

    added swaggerUI

--- Startup.cs
+++ Startup.cs
@@ -26,6 +26,7 @@ namespace TodoAPI
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            services.AddSwaggerGen();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -35,7 +36,12 @@ namespace TodoAPI
             {
                 app.UseDeveloperExceptionPage();
             }
+            app.UseSwagger();
 
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+            });
             app.UseHttpsRedirection();
 
             app.UseRouting();
