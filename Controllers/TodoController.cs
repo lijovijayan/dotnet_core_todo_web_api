da58f4f30c1d31c975132010db20ba72e656fc60
lijovijayan <lijovijayan00@gmail.com>
2020-10-21 14:16:38 +0530

    deleted bin and debug

--- Controllers/TodoController.cs
+++ Controllers/TodoController.cs
@@ -1,16 +1,20 @@
 using System;
+using System.Collections.Generic;
 using Microsoft.AspNetCore.Mvc;
 using TodoAPI.Data;
 using TodoAPI.Models;
 
-namespace TodoAPI.Controllers {
+namespace TodoAPI.Controllers
+{
     [ApiController]
     [Route("api/[controller]")]
-    public class TodoListController: ControllerBase {
-         // /api/TodoList
+    public class TodoListController : ControllerBase
+    {
+        // /api/TodoList
         [HttpGet]
-        public Task Get() {
-            return new TaskRepo().GetTaskById(0);
+        public IEnumerable<Task> Get()
+        {
+            return new TaskRepo().GetTaskList();
         }
     }
 }
